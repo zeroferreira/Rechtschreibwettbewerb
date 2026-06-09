ac7c8f7fba2843c7cb38e682ee6b75b534daf39d
Mario Ferreira <zeroferreira@MacBook-Pro-de-Zero.local>
2026-06-09 09:36:21 -0600

    Configure manifest.json and register sw.js for PWA installability in Chrome

---
+++ English/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Required to satisfy Chrome's PWA install criteria
+});
